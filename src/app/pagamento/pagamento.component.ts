4831127f00e8711655692572e1f6769ebebcee97
Esther Cecim <esther.souza@ecommet.com.br>
2018-10-22 18:46:48 -0300

    Fix: modal pagamento

--- src/app/pagamento/pagamento.component.ts
+++ src/app/pagamento/pagamento.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '../../../node_modules/@ng-bootstrap/ng-bootstrap';
+import { Router } from '../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-pagamento',
@@ -8,17 +9,25 @@ import { NgbModal } from '../../../node_modules/@ng-bootstrap/ng-bootstrap';
 })
 export class PagamentoComponent implements OnInit {
 
-  constructor(private modalService: NgbModal) { }
+  url: string;
+  constructor(private modalService: NgbModal, private route: Router) { }
 
   ngOnInit() {
     // pagar('https://www.mercadopago.com/mlb/checkout/start?pref_id=166987208-c9781467-4354-4552-9e4f-ccf5b066a156')
+    document.getElementById('pagamento-view').hidden = true;
   }
 
   pagar(link: string) {
     location.href = link;
   }
 
-  openVerticallyCentered(content) {
-    this.modalService.open(content, { centered: true });
+  showPagamento(){
+    document.getElementById('pagamento-view').hidden = false;
+    document.getElementById('div-pagamento').hidden = true;
+  }
+
+  voltar(){
+    document.getElementById('pagamento-view').hidden = true;
+    document.getElementById('div-pagamento').hidden = false;
   }
 }
