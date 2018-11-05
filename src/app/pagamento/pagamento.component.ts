da67a0765e5995f9e103fe9f746a4888cabde1ec
Esther Cecim <esther.souza@ecommet.com.br>
2018-11-05 16:39:26 -0200

    Fix: Pagamento

--- src/app/pagamento/pagamento.component.ts
+++ src/app/pagamento/pagamento.component.ts
@@ -10,24 +10,44 @@ import { Router } from '../../../node_modules/@angular/router';
 export class PagamentoComponent implements OnInit {
 
   url: string;
+  saldo = 0.00;
   constructor(private modalService: NgbModal, private route: Router) { }
 
   ngOnInit() {
     // pagar('https://www.mercadopago.com/mlb/checkout/start?pref_id=166987208-c9781467-4354-4552-9e4f-ccf5b066a156')
-    document.getElementById('pagamento-view').hidden = true;
+    this.saldo = JSON.parse(localStorage.getItem('saldo'));
+
+    if(this.saldo === null){
+      this.saldo = 0.00;
+    }
+    console.log(this.saldo);
   }
 
-  pagar(link: string) {
-    location.href = link;
+  pagar20(){
+    this.saldo = this.saldo + 20.00;
+    localStorage.setItem('saldo', JSON.stringify(this.saldo));
+  }
+
+  pagar100(){
+    this.saldo = this.saldo + 100.00;
+    localStorage.setItem('saldo', JSON.stringify(this.saldo));
+  }
+
+  pagar50(link: string) {
+    window.open(
+      link,
+      '_blank' // <- This is what makes it open in a new window.
+    );
+
+    this.saldo = this.saldo + 50.00;
+    localStorage.setItem('saldo', JSON.stringify(this.saldo));
   }
 
   showPagamento(){
-    document.getElementById('pagamento-view').hidden = false;
     document.getElementById('div-pagamento').hidden = true;
   }
 
   voltar(){
-    document.getElementById('pagamento-view').hidden = true;
     document.getElementById('div-pagamento').hidden = false;
   }
 }
